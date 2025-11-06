fbd650e107909682e3cfd61798e2f542df240293
AryanAjayVyawahare <aniketvyawahare891@gmail.com>
2025-11-07 00:20:58 +0530

    Added Wedgets component

---
+++ stocks-app/components/hooks/UseTreadingViewWedgets.tsx
@@ -0,0 +1,10 @@
+`use client`;
+import { useEffect,useRef } from "react";
+
+const UseTreadingViewWedgets = () => {
+  return (
+    <div>UseTreadingViewWedgets</div>
+  )
+}
+
+export default UseTreadingViewWedgets
