9a6579088fa3a67a2fc38b0f64991f05018fc597
AryanAjayVyawahare <aniketvyawahare891@gmail.com>
2025-11-11 23:17:32 +0530

    working on wedget

--- stocks-app/app/(root)/page.tsx
+++ stocks-app/app/(root)/page.tsx
@@ -4,14 +4,14 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-  //     <div className="flex min-h-screen home-wrapper" >
-  //  <section className="grid w-full gap-8 home-section">
-  //   <div className="md:col-span-1 xl:col-span-1">
-  //     <TradingViewWedgets/>
-  //   </div>
-  //  </section>
-  //   </div>
-  <h1>working</h1>
+      <div className="flex min-h-screen home-wrapper" >
+   <section className="grid w-full gap-8 home-section">
+    <div className="md:col-span-1 xl:col-span-1">
+         <h1>Working </h1>
+    </div>
+   </section>
+    </div>
+ 
   )
 }
 
