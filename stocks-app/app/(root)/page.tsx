1c5c87f88347b941894ca75baa88900cb2accb23
AryanAjayVyawahare <aniketvyawahare891@gmail.com>
2025-11-11 23:15:20 +0530

    working on wedget

--- stocks-app/app/(root)/page.tsx
+++ stocks-app/app/(root)/page.tsx
@@ -1,11 +1,17 @@
+import TradingViewWedgets from "@/components/TradingViewWedgets"
 import { Button } from "@/components/ui/button"
 
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen home-wrapper" >
-      Home
-    </div>
+  //     <div className="flex min-h-screen home-wrapper" >
+  //  <section className="grid w-full gap-8 home-section">
+  //   <div className="md:col-span-1 xl:col-span-1">
+  //     <TradingViewWedgets/>
+  //   </div>
+  //  </section>
+  //   </div>
+  <h1>working</h1>
   )
 }
 
