f96da87851492d171f7eb085d877cfabb0ad2d68
AryanAjayVyawahare <aniketvyawahare891@gmail.com>
2025-11-18 22:23:43 +0530

    auth layout created

---
+++ stocks-app/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+     
+    </main>
+  );
+};
+
+export default Layout;
