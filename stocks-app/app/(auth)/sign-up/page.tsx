d1bc06a39778add6ec3a9d776c5eb3e3477827b5
AryanAjayVyawahare <aniketvyawahare891@gmail.com>
2025-12-26 21:14:43 +0530

    Added Interfaces and  and auth logic

--- stocks-app/app/(auth)/sign-up/page.tsx
+++ stocks-app/app/(auth)/sign-up/page.tsx
@@ -1,19 +1,43 @@
+"use client"
+
+import { log } from "console";
 import { useForm } from "react-hook-form"
 
 const SignUp = () => {
 
-  //  const {
-  //   register,
-  //   handleSubmit,
-  //   control,
-  //   formState: { errors, isSubmitting },
-  // } = useForm<SignUpFormData>()
-  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+   const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+       fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: 'Growth',
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology',
+    },
+        mode: 'onBlur'
+  },);
+  const onSubmit= async (data:SignUpFormData) => {
+  try{
+  console.log("Sign-Up Data:", data);
+  }catch(e){
+    console.error("Sign-Up Error:", e);
+
+  }
   return (
     <>
     <h1 className="form-title">Sign-Up & Personalize</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}  className="space-y-5"></form>
+
     </>
   )
+}  
 }
-
-export default SignUp
+ 
+export default SignUp;
